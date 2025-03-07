073ff09b22a4d086ec0373dc60ee20a3d2bd3a42
Atanas Kordov <nasikordov@gmail.com>
2025-03-07 12:41:02 +0200

    task 2 almost done

---
+++ 07-03-PR/Task1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+//int** createEmptyMatrix()
+//{
+//
+//}
+//
+//void deleteMatrix(int** matrix)
+//{
+//
+//}
+
+//int fileSize(std::ifstream& ifs)
+//{
+//	if (!ifs.is_open())
+//	{
+//		std::cerr << "Can't open file.";
+//		return -1;
+//	}
+//
+//	int size = 0;
+//	ifs.seekg(std::ios::end);
+//
+//	size = ifs.tellg();
+//	ifs.seekg(std::ios::beg);
+//
+//	return size;
+//}
+//
+//int main()
+//{
+//	std::ifstream ifs("fst.txt");
+//}
---
+++ 07-03-PR/Task2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+
+void replaceCharsInFile(const char* fileName, char toRep, char repWith)
+{
+	std::ifstream ifs(fileName);
+	if (!ifs.is_open())
+	{
+		std::cerr << "open't\n";
+		return;
+	}
+
+	ifs.seekg(std::ios::end);
+
+	int strSize = ifs.tellg();
+	char* str = new char[strSize];
+
+	ifs.seekg(std::ios::beg);
+
+	char curCh = '.';
+	int id = 0;
+	while (!ifs.eof())
+	{
+		ifs >> curCh;	
+		if (curCh == toRep)
+		{
+			str[id] = repWith;
+		}
+		else
+		{
+			str[id] = curCh; // ?
+		}
+		id++;
+	}
+
+	ifs.close();
+
+	std::ofstream ofs(fileName);
+	if (!ofs.is_open())
+	{
+		std::cerr << "open't\n";
+		return;
+	}
+
+	ofs.write(str, strSize);
+
+	ofs.close();
+}
+
+int main()
+{
+	char toRep = 'g', repWith = '-';
+
+	replaceCharsInFile("replace.txt", toRep, repWith);
+}
